291478659d587dbc59f2fd7826dba017123aad9e
Cristi Petre <driimus@vivaldi.net>
2021-09-03 04:19:44 +0100

    Revert "test: no explicit disconnect"
    This reverts commit 87acec4fad6fb1184461d7075a04c816c4aac599.

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,5 +1,9 @@
 const prisma = require("../src");
 
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
 it("might leak", async () => {
   await prisma.user.deleteMany();
   const user = await prisma.user.create({
