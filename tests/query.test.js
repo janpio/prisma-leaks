22b4d29d4c7e6c0ad49010ded5518df487746475
Cristi Petre <driimus@vivaldi.net>
2021-09-17 23:29:47 +0100

    test: large query count with more data

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,16 +1,93 @@
-const prisma = require("../src");
+const factories = require("../src/factories");
+const prisma = require("../src/index");
+const Factory = require("factory.ts");
+const { sampleSize } = require("lodash");
+
+const seeded = {
+  customUserAttr: Factory.Async.makeFactoryFromSync(
+    factories.customUserAttrFactory.builder
+  ).transform((data) => prisma.user_custom_attributes.create({ data })),
+  userStatus: Factory.Async.makeFactoryFromSync(
+    factories.userStatusFactory.builder
+  ).transform((data) => prisma.user_statuses.create({ data })),
+  user: Factory.Async.makeFactoryFromSync(
+    factories.userFactory.builder
+  ).transform((data) => prisma.users.create({ data })),
+};
 
 afterAll(async () => {
   await prisma.$disconnect();
 });
 
-it("might leak", async () => {
-  await prisma.users.deleteMany();
-  const user = await prisma.users.create({
-    data: {
-      email: "test",
-      projects_limit: 99,
-    },
+describe("users", () => {
+  beforeEach(async () => {
+    await prisma.users.deleteMany();
   });
-  expect(user).toBeTruthy();
+
+  it("should be created without relations", async () => {
+    const user = await seeded.user.build({
+      user_custom_attributes: undefined,
+      user_statuses: undefined,
+    });
+    expect(user).toBeTruthy();
+  });
+
+  it("should be created with relations", async () => {
+    const userList = await seeded.user.buildList(50);
+    const randomUserEmails = sampleSize(userList, 15).map(({ email }) => email);
+    await expect(seeded.user.buildList(5)).resolves.toHaveLength(5);
+    await expect(
+      prisma.users.findMany({
+        where: {
+          email: { in: randomUserEmails },
+        },
+        include: {
+          application_settings: true,
+          ci_triggers: {
+            include: {
+              projects: {
+                include: {
+                  ci_builds: true,
+                },
+              },
+            },
+          },
+          user_statuses: true,
+          user_custom_attributes: true,
+        },
+      })
+    ).resolves.toHaveLength(15);
+  });
+
+  it("should support related field updates", async () => {
+    const userList = await seeded.user.buildList(500);
+    const randomUserEmails = sampleSize(userList, 20).map(({ email }) => email);
+
+    const customUserAttrKeys = (
+      await prisma.user_custom_attributes.findMany({
+        where: {
+          users: {
+            email: { notIn: randomUserEmails },
+          },
+        },
+      })
+    ).map(({ key }) => key);
+
+    await Promise.all(
+      randomUserEmails.map(async (email) => {
+        return prisma.users.update({
+          where: { email },
+          data: {
+            user_custom_attributes: {
+              deleteMany: {
+                key: {
+                  in: customUserAttrKeys,
+                },
+              },
+            },
+          },
+        });
+      })
+    );
+  }, 10000);
 });
