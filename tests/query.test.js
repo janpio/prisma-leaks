429139186c91b303cbc819654ddd60f58d551e04
Cristi Petre <driimus@vivaldi.net>
2022-01-26 21:38:39 +0000

    chore: clean up deps

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,15 +1,10 @@
 const prisma = require("../src/index");
 
-// afterAll(async () => {
-//   await prisma.$disconnect();
-// });
+afterAll(async () => {
+  await prisma.$disconnect();
+});
 
 it("might leak", async () => {
-  await prisma.user.deleteMany();
-  // const user = await prisma.user.create({
-  //   data: {
-  //     email: "test",
-  //   },
-  // });
-  expect(user).toBeTruthy();
+  await prisma.$connect();
+  expect(true).toBeTruthy();
 });
